016e4e4a0a658728d81181cb6f6c309d965018f8
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-06-04 11:41:23 +0200

    Upgrade React@17

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,5 @@
 import { configure } from "enzyme"
-import Adapter from "enzyme-adapter-react-16"
+// TODO: Use official enzyme-adapter-react-17 when available (https://github.com/enzymejs/enzyme/issues/2429)
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17"
 
 configure({ adapter: new Adapter() })
